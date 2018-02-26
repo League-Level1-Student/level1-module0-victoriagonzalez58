cf118f479a973b4b429ffca63d745aa852edd0a7
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2018-02-26 14:22:19 -0800

    Add files via upload

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -31,9 +31,8 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
-
 		for (int i = 0; i < 4; i++) {
 			
 			// 4. Ask the user who this person is and store their answer
